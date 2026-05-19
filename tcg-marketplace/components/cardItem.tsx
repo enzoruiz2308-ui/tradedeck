b7f3be7c6bd93be0ad7643aa0474350322d274a3
Marc Ayala <ayalus.99@gmail.com>
2026-05-19 12:20:25 +0200

    Estructura de pantallas y ejemplos componentes reutilizables

---
+++ tcg-marketplace/components/cardItem.tsx
@@ -0,0 +1,11 @@
+// components/CardItem.tsx
+
+import { View, Text, Image } from 'react-native';
+
+export default function CardItem() {
+  return (
+    <View>
+      <Text>Pikachu</Text>
+    </View>
+  );
+}
