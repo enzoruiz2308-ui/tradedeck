8b4094bb4bff5c45fb983cbf4ef8236a912e4a4d
iker <nengoflow2012sexmachine@gmail.com>
2026-05-21 08:25:05 +0200

    feat: add flask API code

---
+++ config.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tradedeck:tradedeck123@172.17.26.141:5432/tradedeckdb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "tradedeck-secreto-cambiar-en-produccion"
