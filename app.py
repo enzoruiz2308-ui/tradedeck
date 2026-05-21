4ed421bc74949b88b03e89cbab7f8d81659fb4c4
iker <nengoflow2012sexmachine@gmail.com>
2026-05-21 13:07:32 +0200

    Readme completo

--- app.py
+++ app.py
@@ -1,44 +1,32 @@
 from flask import Flask
-
+from flasgger import Swagger
 from config import Config, db, jwt
-
 from routes.usuario_routes import usuario_bp
-
 from routes.anuncio_routes import anuncio_bp
-
 from routes.carta_routes import carta_bp
- 
+
 def create_app():
-
     app = Flask(__name__)
-
     app.config.from_object(Config)
- 
+    app.config["SWAGGER"] = {"title": "TradeDeck API", "uiversion": 3}
+
     db.init_app(app)
-
     jwt.init_app(app)
- 
+    Swagger(app)
+
     app.register_blueprint(usuario_bp)
-
     app.register_blueprint(anuncio_bp)
-
     app.register_blueprint(carta_bp)
- 
+
     with app.app_context():
-
         db.create_all()
- 
+
     @app.route('/')
-
     def index():
-
         return 'TradeDeck API funcionando'
- 
+
     return app
- 
+
 if __name__ == "__main__":
-
     app = create_app()
-
     app.run(host='0.0.0.0', port=8000, debug=True)
- 
